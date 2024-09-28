109e4f541b90dc78ea764af98cfeb36bd6189237
Said <said@wave.com>
2024-09-28 18:53:21 -0400

    add fred api

---
+++ fred/main.py
@@ -0,0 +1,26 @@
+import requests
+
+api_key = '530aa1f8b29b07bf2c0901001ebe7624'
+
+def construct_url(endpoint, series_id, realtime_start, realtime_end, api_key, filetype):
+    base_url = 'https://api.stlouisfed.org'
+    return f"{base_url}/{endpoint}?series_id={series_id}&realtime_start={realtime_start}&&realtime_end={realtime_end}&api_key={api_key}&file_type={filetype}"
+
+def send_get_request(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"{response.status_code} and text is {response.text}")
+
+if __name__ == "__main__":
+    endpoint = 'fred/series'
+    series_id = 'DEXUSEU'
+    realtime_start = '2000-01-01'
+    realtime_end = '2024-09-22'
+    filetype ='json'
+    url = construct_url(endpoint, series_id, realtime_start, realtime_end, api_key, filetype)
+    response = send_get_request(url)
+    print(url)
+    print(response)
